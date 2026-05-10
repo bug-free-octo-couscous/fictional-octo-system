0ddec4fe7df753733d8fbb3bb79ceb35231c7841
jihoo <jihoop0620@gmail.com>
2026-05-10 19:21:43 +0900

    update type definition

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,14 @@
+data Var
+    = N String
+    | Num Int
+    deriving (Show)
 data Form
-    = Formula Form
-    | Val Form Form
-    | X Int
-    | Dim Int
-    | T Form String
-    | Fill Form Form
+    = Val [Form]
+    | X Int Var
+    | E Form String
     deriving (Show)
 
-myFormula1 :: Form
-myFormula1 = Formula (T (Val (Dim 1) (X 1)) "a1") -- 1d point
-myFormula2 :: Form
-myFormula2 = Formula (T (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 2))) "a2") -- 2d point
-myFormula3 :: Form
-myFormula3 = Formula (T (Val (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 1))) (Val (Dim 3) (X 1))) "a3") -- 3d point
-myFormula12 :: Form
-myFormula12 = Formula (T (Val (Val (Dim 1) (X 1)) (Val (Dim 1) (X 2))) "a12") -- line
-mycube1 :: Form
-mycube1 = Formula (Fill (Val (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 2))) (Val (Dim 3) (X 1))) (Val (Val (Val (Dim 1) (X 2)) (Val (Dim 2) (X 2))) (Val (Dim 3) (X 3)))) -- cube
-main :: IO ()
-main = do
-    print myFormula1
-    print myFormula2
-    print myFormula3
-    print myFormula12
-    print mycube1
+myval :: Form
+myval = (E (Val [(X 1 (Num 1))]) "myval") -- myval = 1
+myline :: Form
+myline = (E (Val [(X 1 (N "n"))]) "myline") -- myline = n 
