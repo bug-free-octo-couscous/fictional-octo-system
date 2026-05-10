e9e1e16ead7559b3faf7b3d22513192eff744bff
jihoo <jihoop0620@gmail.com>
2026-05-10 13:20:41 +0900

    Form

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,19 @@
-module Main (main) where
+data Form
+    = Formula Form Form
+    | Name String
+    | Val Form Form
+    | X Int
+    | Dim Int
+    deriving (Show)
+
+myFormula1 :: Form
+myFormula1 = Formula (Name "a") (Val (X 1) (Dim 1))
+myFormula2 :: Form
+myFormula2 = Formula (Name "b") (Val (X 1) (Dim 1))
+myFormula3 :: Form
+myFormula3 = Formula (myFormula1) (myFormula2)
+myFormula4 :: Form 
+myFormula4 = Formula (Name "c") (myFormula3)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print myFormula4
