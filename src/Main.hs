6a5a914c122557a2802c87487819a733525115e0
jihoo <jihoop0620@gmail.com>
2026-05-10 14:01:16 +0900

    form

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,26 @@
 data Form
-    = Formula Form Form
-    | Name String
+    = Formula Form
     | Val Form Form
     | X Int
     | Dim Int
+    | T Form String
+    | Fill Form Form
     deriving (Show)
 
 myFormula1 :: Form
-myFormula1 = Formula (Name "a") (Val (X 1) (Dim 1))
+myFormula1 = Formula (T (Val (Dim 1) (X 1)) "a1") -- 1d point
 myFormula2 :: Form
-myFormula2 = Formula (Name "b") (Val (X 1) (Dim 1))
+myFormula2 = Formula (T (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 2))) "a2") -- 2d point
 myFormula3 :: Form
-myFormula3 = Formula (myFormula1) (myFormula2)
-myFormula4 :: Form 
-myFormula4 = Formula (Name "c") (myFormula3)
-
+myFormula3 = Formula (T (Val (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 1))) (Val (Dim 3) (X 1))) "a3") -- 3d point
+myFormula12 :: Form
+myFormula12 = Formula (T (Val (Val (Dim 1) (X 1)) (Val (Dim 1) (X 2))) "a12") -- line
+mycube1 :: Form
+mycube1 = Formula (Fill (Val (Val (Val (Dim 1) (X 1)) (Val (Dim 2) (X 2))) (Val (Dim 3) (X 1))) (Val (Val (Val (Dim 1) (X 2)) (Val (Dim 2) (X 2))) (Val (Dim 3) (X 3)))) -- cube
 main :: IO ()
-main = print myFormula4
+main = do
+    print myFormula1
+    print myFormula2
+    print myFormula3
+    print myFormula12
+    print mycube1
