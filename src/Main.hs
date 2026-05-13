bddd664297fc3087917a150989f6e7c9ae2c2e56
jihoo <jihoop0620@gmail.com>
2026-05-13 09:52:35 +0900

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+module Main where
 data Var
     = N String
     | Num Int
@@ -5,10 +6,22 @@ data Var
 data Form
     = Val [Form]
     | X Int Var
+    | I Int Var
     | E Form String
     deriving (Show)
 
 myval :: Form
-myval = (E (Val [(X 1 (Num 1))]) "myval") -- myval = 1
+myval = (E (Val [(X 1 (Num 1))]) "myval") -- (1) R^1
+mypoint :: Form
+mypoint = (E (Val [(X 1 (Num 1)), (X 2 (Num 1)), (X 3 (Num 1))]) "mypoint3d") -- (1,1,1) R^3
+mypoint_complex_plane :: Form
+mypoint_complex_plane = (E (Val [(X 1 (Num 1)), (I 1 (Num 1))]) "mypoint_complex_plane") -- (1+i) C^1
+mypoint_complex_plane3d :: Form
+mypoint_complex_plane3d = (E (Val [(X 1 (Num 1)), (I 1 (Num 1)), (X 2 (Num 1)), (I 2 (Num 1)), (X 3 (Num 1)), (I 3 (Num 1))]) "mypoint_complex_plane") -- (1+i,1+i,1+i) C^3
+mypoint_complex_plane_more_imaginary :: Form
+mypoint_complex_plane_more_imaginary = (E (Val [(X 1 (Num 1)), (I 1 (Num 1)), (I 2 (Num 1))]) "mypoint_complex_plane") -- (1+i+j) R^2 + I^2
+
 myline :: Form
 myline = (E (Val [(X 1 (N "n"))]) "myline") -- myline = n 
+main :: IO ()
+main = print myline
