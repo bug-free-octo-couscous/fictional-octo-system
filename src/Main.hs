9eb76e29ce1e17a2deba4cfc3119f8dff23ebcb2
jihoo <jihoop0620@gmail.com>
2026-05-13 09:55:35 +0900

    update

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,9 @@ data Form
     | E Form String
     deriving (Show)
 
+data Comp
+    = CompE Form Form String
+
 myval :: Form
 myval = (E (Val [(X 1 (Num 1))]) "myval") -- (1) R^1
 mypoint :: Form
@@ -20,8 +23,9 @@ mypoint_complex_plane3d :: Form
 mypoint_complex_plane3d = (E (Val [(X 1 (Num 1)), (I 1 (Num 1)), (X 2 (Num 1)), (I 2 (Num 1)), (X 3 (Num 1)), (I 3 (Num 1))]) "mypoint_complex_plane") -- (1+i,1+i,1+i) C^3
 mypoint_complex_plane_more_imaginary :: Form
 mypoint_complex_plane_more_imaginary = (E (Val [(X 1 (Num 1)), (I 1 (Num 1)), (I 2 (Num 1))]) "mypoint_complex_plane") -- (1+i+j) R^2 + I^2
-
+myComp :: Comp
+myComp = (CompE myval mypoint "my_new_space")
 myline :: Form
 myline = (E (Val [(X 1 (N "n"))]) "myline") -- myline = n 
 main :: IO ()
-main = print myline
+main = print mypoint_complex_plane_more_imaginary
